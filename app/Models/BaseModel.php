ad26de7957dc88851cb3c7dc1c06ead9895f337c
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2015-10-22 16:19:59 +0800

    ui

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -73,7 +73,7 @@ class BaseModel extends Model {
 
 
 	public function getDaysByWeekNo($weekno='', $year=''){
-  	$weekno = (empty($weekno) || $ > $this->lastWeekOfYear()) ? date('W', strtotime('now')) : $weekno;
+  	$weekno = (empty($weekno) || $weekno > $this->lastWeekOfYear()) ? date('W', strtotime('now')) : $weekno;
   	$year = empty($year) ?  $this->year : $year;
 		for($day=1; $day<=7; $day++) {
 		    $arr[$day-1] = date('Y-m-d', strtotime($year."W".str_pad($weekno,2,'0',STR_PAD_LEFT).$day));
