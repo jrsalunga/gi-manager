f25ab4af6c4aa3b2eb6386f8227f3369b4ce00d2
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-04-07 16:57:19 +0800

    fix

--- resources/views/dashboard/analytics.blade.php
+++ resources/views/dashboard/analytics.blade.php
@@ -163,7 +163,7 @@
             <tr {{ $d->date->dayOfWeek=='0' ? 'class=warning':''  }}>
               <td data-sort="{{$d->date->format('Y-m-d')}}">{{ $d->date->format('M j, D') }}</td>
               @if(!is_null($d->dailysale))
-              <td class="text-right" data-sort="{{ number_format($d->dailysale['sales'], 2,'.','') }}">{{ dd(number_format($d->dailysale['sales'], 2)) }}</td>
+              <td class="text-right" data-sort="{{ number_format($d->dailysale['sales'], 2,'.','') }}">{{ number_format($d->dailysale['sales'], 2) }}</td>
               <td class="text-right" data-sort="{{ number_format($d->dailysale['purchcost'], 2,'.','') }}">{{ number_format($d->dailysale['purchcost'], 2) }}</td>
               <td class="text-right" data-sort="{{ number_format($d->dailysale['custcount'], 0) }}">{{ number_format($d->dailysale['custcount'], 0) }}</td>
               <td class="text-right" data-sort="{{ number_format($d->dailysale['headspend'], 2,'.','') }}">{{ number_format($d->dailysale['headspend'], 2) }}</td>
@@ -178,7 +178,7 @@
               ?>
               <td class="text-right" data-sort="{{ number_format($mancost,2,'.','') }}">{{ number_format($mancost,2) }}</td>
               <td class="text-right" data-sort="{{ $d->dailysale['mancostpct'] }}"
-                @if($d->dailysale['sales']!='0.00' || $d->dailysale['sales']!='0')
+                @if($d->dailysale['sales']!='0.00' || $d->dailysale['sales']!='0' || !is_null($d->dailysale['sales']) )   
                 title="({{$d->dailysale['empcount']}}*{{session('user.branchmancost')}})/{{$d->dailysale['sales']}} 
                 ={{(($d->dailysale['empcount']*session('user.branchmancost'))/$d->dailysale['sales'])*100}}"
                 @endif
