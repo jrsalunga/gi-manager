a4ba450be1e1097140d9cd81807882ac7a8ce3d4
Jefferson Salunga <jefferson.salunga@gmail.com>
2017-03-01 19:33:26 +0800

    fix emp dash

--- resources/views/employee/dashboard.blade.php
+++ resources/views/employee/dashboard.blade.php
@@ -60,7 +60,7 @@
           </tr>
         @endforeach
         </tbody>
-        <thead><tr><td></td><td>{{ $data['positions']['total'] }}</td></tr></thead>
+        <thead><tr><td></td><td><a href="/{{brcode()}}/employee">{{ $data['positions']['total'] }}</a></td></tr></thead>
         </table>
       @endif
     </div>
